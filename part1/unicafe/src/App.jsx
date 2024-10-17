28a6d8fbd1f729a678e4e310e7ecf1b9f02760a9
MNummila <maxnummila@gmail.com>
2024-10-17 11:12:26 +0300

    Completed the feedback unicafe task and added the needed css template for the HTML table

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,25 +1,51 @@
 import { useState } from 'react'
 
-
+//Takes a text as a parameter and converts it to a header
 const Header = ({ text }) => {
     return (
         <h1>{text}</h1>
     );
 };
+
+//Takes a function handleClick and a text as parameters and creates a button with the functionality of the function and the name of the text
 const Button = ({ handleClick, text }) => (
     <button onClick={handleClick}>
         {text}
     </button>
 )
 
-const StatisticsDisplayer = ({ name, amount }) => (
-    <p>{name} {amount}</p>
+//Takes a text and a value as parameters and makes a tablecomponent with the corresponding name and value
+const StatisticsLine = ({ text, value }) => (
+    <tr>
+        <td>{text}</td>
+        <td>{value}</td>
+    </tr>
 );
 
-const Statistics = (props) => {
+//Takes all the different types of statistics wanted as parameter and creates an HTML table with these statistics using the StatisticsLine component
+const Statistics = ({ good, neutral, bad, total, average, positive }) => {
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr>
 
+                    </tr>
+                </thead>
+                <tbody>
+                    <StatisticsLine text='good' value={good} />
+                    <StatisticsLine text='neutral' value={neutral} />
+                    <StatisticsLine text='bad' value={bad} />
+                    <StatisticsLine text='total' value={total} />
+                    <StatisticsLine text='average' value={average} />
+                    <StatisticsLine text='positive' value={`${positive}%`} />
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
+//The app component itself, contains all the button functionality, calculations for average and positive feedback and returns the page HTML
 const App = () => {
     // save clicks of each button to its own state
     const [good, setGood] = useState(0)
@@ -35,6 +61,7 @@ const App = () => {
     const statistics = {
         name: 'statistics'
     }
+    let statisticsDisplay;
 
     const calculateAverage = (updatedGood, updatedNeutral, updatedBad) => {
         const totalScore = (updatedGood * 1) + (updatedNeutral * 0) + (updatedBad * -1);
@@ -80,6 +107,16 @@ const App = () => {
         setPositive(calculatePositive(good, neutral, updatedBad))
     }
 
+    if(total === 0){
+        statisticsDisplay = <p>No feedback given</p>
+    } else{
+        statisticsDisplay = (
+            <Statistics
+                good={good} neutral={neutral} bad={bad} average={average} positive={positive} total={total}
+            />
+        );
+    }
+
 
     return (
         <div>
@@ -88,12 +125,7 @@ const App = () => {
             <Button handleClick={handleNeutralClick} text='neutral' />
             <Button handleClick={handleBadClick} text='bad' />
             <Header text={statistics.name} />
-            {StatisticsDisplayer({name: 'good', amount: good})}
-            {StatisticsDisplayer({ name: 'neutral', amount: neutral })}
-            {StatisticsDisplayer({ name: 'bad', amount: bad })}
-            {StatisticsDisplayer({name: 'total', amount: total})}
-            {StatisticsDisplayer({name: 'average', amount: average})}
-            {StatisticsDisplayer({name: 'positive', amount: `${positive}%`})}
+            {statisticsDisplay}
         </div>
     )
 }
