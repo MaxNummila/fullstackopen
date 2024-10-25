efaa61e45a91e75cff35328eeb1f34ef7dc1666e
MNummila <maxnummila@gmail.com>
2024-10-25 21:26:45 +0300

    Anecdotes part2

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -14,16 +14,16 @@ function getRandomInt(min, max) {
 }
 
 const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
+  const [anecdotes, setAnecdotes] = useState([
+    {text: 'If it hurts, do it more often.', points: 0},
+    {text: 'Adding manpower to a late software project makes it later!', points: 0},
+    {text: 'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.', points: 0},
+    {text: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.', points: 0},
+    {text: 'Premature optimization is the root of all evil.', points: 0},
+    {text: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.', points: 0},
+    {text: 'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.', points: 0},
+    {text: 'The only way to go fast, is to go well.', points: 0}
+  ]);
    
   const [selected, setSelected] = useState(getRandomInt(0, anecdotes.length))
 
@@ -32,11 +32,22 @@ const App = () => {
     setSelected(randomIndex);
   }
 
+  const handleVoteClick = () => {
+    const updatedAnecdotes = anecdotes.map((anecdote, index) =>
+      index === selected ? { ...anecdote, points: anecdote.points + 1} : anecdote
+    );
+
+    setAnecdotes(updatedAnecdotes);
+  }
+
   return (
     <div>
-      {anecdotes[selected]}
+      {anecdotes[selected].text}
+      <br/>
+      {anecdotes[selected].points}
       <br/>
       <Button handleClick={handleAnecdoteClick} text='New anecdote'/>
+      <Button handleClick={handleVoteClick} text='Vote for this one'/>
     </div>
   )
 }
