61b9f65ccc935bd9a0ad51be55ba15ccebdc339b
MNummila <maxnummila@gmail.com>
2025-10-01 18:23:52 +0300

    Update App.jsx

--- part2/note-frontend/src/App.jsx
+++ part2/note-frontend/src/App.jsx
@@ -16,6 +16,18 @@ const App = () => {
 			})
 	}, [])
 
+	const toggleImportanceOf = id => {
+		const url = `http://localhost:3001/notes/${id}`
+		const note = notes.find(n => n.id === id)
+		const changedNote = { ...note, important: !note.important }
+
+		noteService
+			.update(id, changedNote)
+			.then(returnedNote => {
+				setNotes(notes.map(note => note.id === id ? returnedNote : note))
+			})
+	}
+
 	const addNote = (event) => {
 		event.preventDefault()
 		const noteObject = {
@@ -28,18 +40,6 @@ const App = () => {
 			.then(returnedNote => {
 				setNotes(notes.concat(returnedNote))
 				setNewNote('')
-			})
-	}
-
-	const toggleImportanceOf = id => {
-		const url = `http://localhost:3001/notes/${id}`
-		const note = notes.find(n => n.id === id)
-		const changedNote = { ...note, important: !note.important }
-
-		noteService
-			.update(id, changedNote)
-			.then(returnedNote => {
-				setNotes(notes.map(note => note.id === id ? returnedNote : note))
 			})
 	}
 
